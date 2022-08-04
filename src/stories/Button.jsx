f6b1c1c5c80e17ef3580bfbd4e49adcf11fd445c
Abi Murugesan <abimurugesan@interviewdesk.in>
2022-08-04 17:09:50 +0530

    "Changes"

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -14,7 +14,7 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
-      {label}Smalll
+      {label}Smalllsssss
     </button>
   );
 };
