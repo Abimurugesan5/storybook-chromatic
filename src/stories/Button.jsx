2727858e21dc2b9e03f118e0c302121cf741f37c
Abi Murugesan <abimurugesan@interviewdesk.in>
2022-08-05 13:25:35 +0530

    add a bug

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -14,7 +14,7 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
-      {label}
+     text before {label}
     </button>
   );
 };
